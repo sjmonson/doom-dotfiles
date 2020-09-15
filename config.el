5957119f921243237206cbdd8625a30bb99ef9b0
scrufulufugus <smonson@irbash.net>
2020-09-15 16:00:08 -0700

    Updated emacs config

--- config.el
+++ config.el
@@ -1,3 +1,23 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+(after! tide
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode +1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (tide-hl-identifier-mode +1)
+    ;; company is an optional dependency. You have to
+    ;; install it separately via package-install
+    ;; `M-x package-install [ret] company`
+    (company-mode +1))
+
+  ;; aligns annotation to the right hand side
+  (setq company-tooltip-align-annotations t)
+)
+;; formats the buffer before saving
+(add-hook! 'before-save-hook 'tide-format-before-save)
+
+(add-hook! 'typescript-mode-hook #'setup-tide-mode)
