f984a1d14b6c05d338b9dcb924697e96d4643b2f
Samuel Monson <smonson@redhat.com>
2025-08-12 13:52:53 -0400

    Format and cleanup config

--- config.el
+++ config.el
@@ -3,23 +3,23 @@
 ;; Removes evil-snipe repeat keys such as ;
 (setq evil-snipe-override-evil-repeat-keys nil)
 ;; Stops evil-snipe from overiding keybinds
-;(after! evil-snipe (evil-snipe-override-mode -1))
+;; (after! evil-snipe (evil-snipe-override-mode -1))
 
-(after! lsp
+(after! lsp-ui
   (setq lsp-ui-sideline-show-code-actions nil)
-)
+  )
 
 ;; Cache passwords forever
 (setq password-cache-expiry nil)
 
-; Adapted from https://www.reddit.com/r/emacs/comments/o49v2w/automatically_switch_emacs_theme_when_changing/?rdt=59586
+;; Adapted from https://www.reddit.com/r/emacs/comments/o49v2w/automatically_switch_emacs_theme_when_changing/?rdt=59586
 (defun mf/set-theme-from-dbus-value (value)
   "Set the appropiate theme according to the color-scheme setting value."
-    (if (equal value '1)
-        (progn (message "Switch to dark theme")
-               (load-theme 'doom-one))
-      (progn (message "Switch to light theme")
-             (load-theme 'doom-one-light))))
+  (if (equal value '1)
+      (progn (message "Switch to dark theme")
+             (load-theme 'doom-one))
+    (progn (message "Switch to light theme")
+           (load-theme 'doom-one-light))))
 
 (defun mf/color-scheme-changed (path var value)
   "DBus handler to detect when the color-scheme has changed."
@@ -27,14 +27,16 @@
              (string-equal var "color-scheme"))
     (mf/set-theme-from-dbus-value (car value))
     ))
+
+;; Register the dbus signal handler for color-scheme changes
 (use-package! dbus
   :config
   ;; Register for future changes
   (dbus-register-signal
-     :session "org.freedesktop.portal.Desktop"
-     "/org/freedesktop/portal/desktop" "org.freedesktop.portal.Settings"
-     "SettingChanged"
-     #'mf/color-scheme-changed)
+   :session "org.freedesktop.portal.Desktop"
+   "/org/freedesktop/portal/desktop" "org.freedesktop.portal.Settings"
+   "SettingChanged"
+   #'mf/color-scheme-changed)
 
   ;; Request the current color-scheme
   (dbus-call-method-asynchronously
@@ -55,9 +57,7 @@
 (after! org
   (use-package! ox-extra
     :config
-    (ox-extras-activate '(latex-header-blocks ignore-headlines))))
-
-(after! org
+    (ox-extras-activate '(latex-header-blocks ignore-headlines)))
   ;; Import ox-latex to get org-latex-classes and other funcitonality
   ;; for exporting to LaTeX from org
   (use-package! ox-latex
@@ -74,8 +74,8 @@
     ;; deleted unwanted file extensions after latexMK
     (setq org-latex-logfiles-extensions
           (quote ("lof" "lot" "tex~" "aux" "idx" "log" "out" "toc" "nav" "snm" "vrb" "dvi"
-                   "fdb_latexmk" "blg" "brf" "fls" "entoc" "ps" "spl" "bbl" "xmpi" "run.xml"
-                   "bcf" "acn" "acr" "alg" "glg" "gls" "ist" "xdv")))
+                  "fdb_latexmk" "blg" "brf" "fls" "entoc" "ps" "spl" "bbl" "xmpi" "run.xml"
+                  "bcf" "acn" "acr" "alg" "glg" "gls" "ist" "xdv")))
 
     (unless (boundp 'org-latex-classes)
       (setq org-latex-classes nil))))
@@ -84,7 +84,6 @@
 (use-package! copilot
   :hook (prog-mode . copilot-mode)
   :bind (:map copilot-completion-map
-              ("C-e" . 'copilot-accept-completion)
               ("C-e" . 'copilot-accept-completion)
               ("C-TAB" . 'copilot-accept-completion-by-word)
               ("C-<tab>" . 'copilot-accept-completion-by-word)))
